50d5adeae29191d66579b8239831aeea262b36da
Adithyan A <69440263+AdithyanA2005@users.noreply.github.com>
2023-07-14 10:21:19 +0000

    Function to connect to mongodb created

---
+++ utils/database.ts
@@ -0,0 +1,24 @@
+import mongoose, { ConnectOptions } from "mongoose";
+
+// Track the connection status
+let isConnected: boolean = false;
+
+// Function to establish the connection
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  // Only continue if connection is not established
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    // Try to connect to the database
+    const connectOptions: ConnectOptions = { dbName: "prompts" };
+    await mongoose.connect(process.env.MONGODB_URI || "", connectOptions);
+  } catch (error) {
+    // Print any error that has occured
+    console.log(error)
+  }
+}
