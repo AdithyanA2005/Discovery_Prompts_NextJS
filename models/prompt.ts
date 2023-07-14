2088d2d964dcfb7ddcfca109f1ae87574472b3d5
Adithyan A <69440263+AdithyanA2005@users.noreply.github.com>
2023-07-14 13:14:08 +0000

    Prompt Model Created

---
+++ models/prompt.ts
@@ -0,0 +1,26 @@
+import { Document } from 'mongodb';
+import { Model, Schema, Types, model, models } from 'mongoose';
+
+export interface IPrompt extends Document {
+  creator: Types.ObjectId;
+  prompt: string;
+  tag: string;
+}
+
+const promptSchema: Schema<IPrompt> = new Schema<IPrompt>({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  prompt: {
+    type: String,
+    required: true,
+  },
+  tag: {
+    type: String,
+    required: true,
+  }
+});
+
+const Prompt: Model<IPrompt> = models.Prompt || model('Prompt', promptSchema);
+export default Prompt;
