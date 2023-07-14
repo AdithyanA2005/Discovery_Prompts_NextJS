7fd31f4cb63ef37d47c0612fcf3d0fa66d079f1e
Adithyan A <69440263+AdithyanA2005@users.noreply.github.com>
2023-07-14 10:23:01 +0000

    MongoDB Users Model Created

---
+++ models/user.ts
@@ -0,0 +1,26 @@
+import { Document } from 'mongodb';
+import { Model, Schema, model, models } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  image: string;
+  email: string;
+}
+
+const userSchema: Schema<IUser> = new Schema<IUser>({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+});
+
+const User: Model<IUser> = models.User || model("User", userSchema);
+export default User;
