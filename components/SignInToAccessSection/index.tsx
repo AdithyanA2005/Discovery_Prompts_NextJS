cd341d3740c6af79d4d724adb944588fdf4629a6
Adithyan A <deepaadithyan56@gmail.com>
2023-07-25 09:19:15 +0530

    SignInToAccessSection design change

--- components/SignInToAccessSection/index.tsx
+++ components/SignInToAccessSection/index.tsx
@@ -15,15 +15,15 @@ export default function SignInToAccessSection({ }: Props) {
 
   return (
     <section className="mt-[10vh] flex items-center justify-center flex-col">
-      <div className="flex flex-col gap-14 items-center border-2 border-primary rounded-2xl bg-black/30 px-14 py-16">
-        <h1 className="text-gray-300 text-4xl font-semibold">
+      <div className="flex flex-col gap-8 md:gap-12 lg:gap-14 items-center border-2 border-primary rounded-2xl bg-black/30 px-8 py-10 md:px-10 md:py-14">
+        <h1 className="text-gray-300 text-2xl md:text-3xl text-center font-semibold">
           Please SignIn to access the page
         </h1>
 
         <button
           onClick={() => signIn(providers?.google.id, { callbackUrl: pathname })}
           disabled={!providers || !providers.google}
-          className="btn btn-primary btn-lg w-full max-w-sm"
+          className="btn btn-primary lg:btn-lg w-full max-w-sm"
         >
           Sign In / Register
         </button>
