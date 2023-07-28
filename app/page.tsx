ba2b207899535721aa24e0d0bda0afefc68ba1e5
Adithyan A <deepaadithyan56@gmail.com>
2023-07-28 20:10:43 +0530

    App desc changed

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function RootPage() {
       <HeroSection
         heading="Discover & Share"
         headingSpecial="AI-Powered Prompts"
-        description="Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts"
+        description="Discovery Prompts is an open-source AI prompting tool for modern world to discover, create and share creative prompts"
       />
 
       {/* Prompt Feeds Section */}
